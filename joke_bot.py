ebb08e7f06d992b17df51a86af54b25b309a8dd3
guessuser <92473513+guessuser@users.noreply.github.com>
2022-05-03 21:39:20 +0300

    Add files via upload

--- joke_bot.py
+++ joke_bot.py
@@ -1,7 +1,16 @@
+from email import message
 import telebot
+from telebot import types
+import requests
+import random
 
 bot = telebot.TeleBot('5396222937:AAE1WU3271i7CfA9R8hesnUPAFXFa1NsSQQ')
+message_list = ['Привет!', 'Привет', 'Здравствуй!', 'Привет', 'Привет!', 'Здравствуй!', 'Привет, я хочу умереть', 'Привет!',  'Привет',  'Здравствуй']
 @bot.message_handler(commands=['start'])
 def start_message (message):
-    bot.send_message(message.chat.id, 'hello')
+    bot.send_message(message.chat.id, message_list[random.randint(0, 9)])
+@bot.message_handler(commands = ['blacklist'])
+def blacklist_func (message):
+    bot.send_message(message.chat.id, 'Настройки черного списка')
+
 bot.polling(non_stop = True)
