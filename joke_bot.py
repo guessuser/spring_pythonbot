80368d37b709f2be348b72a8d6429a1fa530efbc
guessuser <92473513+guessuser@users.noreply.github.com>
2022-05-04 01:26:17 +0300

    Add files via upload

--- joke_bot.py
+++ joke_bot.py
@@ -1,14 +1,15 @@
-from email import message
 import telebot
 from telebot import types
-import requests
+import requests 
 import random
-
-bot = telebot.TeleBot('5396222937:AAE1WU3271i7CfA9R8hesnUPAFXFa1NsSQQ')
-message_list = ['Привет!', 'Привет', 'Здравствуй!', 'Привет', 'Привет!', 'Здравствуй!', 'Привет, я хочу умереть', 'Привет!',  'Привет',  'Здравствуй']
+#токен привязывающий бота
+bot = telebot.TeleBot('5396222937:AAE1WU3271i7CfA9R8hesnUPAFXFa1NsSQQ') 
+#служебные списки
+message_list = ['Привет!', 'Привет', 'Здравствуй!', 'Привет', 'Привет!', 'Здравствуй!', 'Привет, я хочу умереть', 'Привет!',  'Привет',  'Здравствуй'] 
 blacklist = []
-joke_list = ['шутка-минутка', 'шутка-часик']
-
+joke_list = ['шутка-минутка', 'шутка-часик'] 
+#обработка команды /start - приветствие и клавиатура для управления.
+#Вообще я думаю поменять эту штуку немного - сделать приветствие + /help??? надо подумать немного над этим вашим дезигном
 @bot.message_handler(commands=['start'])
 def start_message (message):
     keyboard = types.InlineKeyboardMarkup()
@@ -18,11 +19,13 @@ def start_message (message):
     keyboard.add(key_search_joke_keyword)
     key_search_joke_cliche = types.InlineKeyboardButton(text = 'Поиск по шаблону', callback_data = 'search_cliche')
     keyboard.add(key_search_joke_cliche)
+    key_hedgehog = types.InlineKeyboardButton(text = 'Анекдот про ёжика', callback_data = 'hedgehog')
+    keyboard.add(key_hedgehog)
+    #а вообще я люблю "Случаи" Хармса, один даже есть в readme и я думала сюда их добавить. 
+    #Но это во-первых, не уважительно к автору, а во вторых это не анекдоты. Хотя гораздо смешнее. Особенно в исполнении моей пассии
     bot.send_message(message.chat.id, message_list[random.randint(0, 9)], reply_markup = keyboard)
-    def random_joke(call):
-        if call.data == 'rand_joke':
-            bot.send_message (message.chat.id, joke_list[random.randint(0,1)])
-
+   
+#обработка команды /blacklist - клавиатура с кнопками
 @bot.message_handler(commands = ['blacklist'])
 def blacklist_settings(message):
     keyboard_bl = types.InlineKeyboardMarkup()
@@ -33,8 +36,19 @@ def blacklist_settings(message):
     key_clear_bl = types.InlineKeyboardButton(text = 'Очистить чёрный список', callback_data = 'clear_blacklist')
     keyboard_bl.add(key_clear_bl)
     bot.send_message(message.chat.id, 'Настройки чёрного списка', reply_markup = keyboard_bl)
-    
+
+#Нет никакой необходимости делать две разные функции для обработки callback но я их разделила чтобы было удобнее работать  
 @bot.callback_query_handler(func = lambda call: True)
+def main_operator(call):
+        if call.data == 'rand_joke':
+            bot.send_message (call.message.chat.id, joke_list[random.randint(0,1)])
+        elif call.data == 'search_keyword':
+            bot.send_message (call.message.chat.id, 'Шутка по ключевым словам В РАЗРАБОТКЕ')
+        elif call.data == 'search_cliche':
+            bot.send_message (call.message.chat.id, 'Заходит как-то шаблонная шутка в бар, а бармен говорит В РАЗРАБОТКЕ')
+        elif call.data == 'hedgehog':
+            bot.send_message (call.message.chat.id, 'Жил-был ёжик, научился он дышать попой. Сел на пенёк и задохнулся') #Это единственная шутка которую я могу сходу рассказать.
+#Я где-то сломала эту функцию и не могу понять где
 def blacklist_operations(call):
         if call.data == 'show_blacklist':
             if len(blacklist) == 0:
@@ -47,5 +61,8 @@ def blacklist_operations(call):
             keyboard_edit.add(key_add_words)
             key_delete_words = types.InlineKeyboardButton (text = 'Удалить', callback_data = 'delete_word')
             keyboard_edit.add(key_delete_words)
+            key_clear_words = types.InlineKeyboardButton (text = 'Очистить чёрный список', callback_data = 'clear_blacklist')
+            keyboard_edit.add(key_clear_words)
             bot.send_message (call.message.chat.id, 'Редактирование чёрного списка', reply_markup = keyboard_edit)
+#отправка запросов в телегу спрашивать ну как там с (деньгами) сообщениями
 bot.polling(non_stop = True)
