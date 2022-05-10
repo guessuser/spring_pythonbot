8ab9bbad3b76311852a4d76d40bff975e71a7280
guessuser <92473513+guessuser@users.noreply.github.com>
2022-05-10 17:45:55 +0300

    Add files via upload

--- joke_bot.py
+++ joke_bot.py
@@ -2,15 +2,23 @@ import telebot
 from telebot import types
 import requests 
 import random
+from bs4 import BeautifulSoup
+
 
 bot = telebot.TeleBot('5396222937:AAE1WU3271i7CfA9R8hesnUPAFXFa1NsSQQ') 
 
 message_list = ['Привет!', 'Привет', 'Здравствуй!', 'Привет', 'Привет!', 'Здравствуй!', 'Привет, я хочу умереть', 'Привет!',  'Привет',  'Здравствуй'] 
-blacklist = ['damn', 'do you think I care?', 'oh man, clearly not']
-joke_list = ['шутка-минутка', 'шутка-часик'] 
 
-def clear_blacklist_function(list):
-    blacklist.clear
+blacklist = ['Bruno', 'Бруно', '''we don't talk about Bruno''', 'bruno', 'бруно']
+joke_list = ['шутка-минутка', 'другая-шутка-минутка', 'сырок', 'другой сырок'] 
+
+def clear_blacklist_function(blacklist):
+    blacklist = blacklist.clear()   
+def add_words_bl_function(blacklist):
+    blacklist = blacklist.append('интересные слова')
+@bot.message_handler(commands=['help'])
+def help_message (message):
+   bot.send_message(message.chat.id, 'Привет! Это чат-бот юморист который травит сомнительные анекдоты.\nКоманда /start запускает бота и выводит в главное меню.\nКоманда /blacklist вызывает настройки черного списка')
 
 @bot.message_handler(commands=['start'])
 def start_message (message):
@@ -40,7 +48,7 @@ def blacklist_settings(message):
 @bot.callback_query_handler(func = lambda call: True)
 def main_operator(call):
         if call.data == 'rand_joke':
-            bot.send_message (call.message.chat.id, joke_list[random.randint(0,1)])
+            bot.send_message (call.message.chat.id, joke_list[random.randint(0,3)])
         elif call.data == 'search_keyword':
             bot.send_message (call.message.chat.id, 'Шутка по ключевым словам В РАЗРАБОТКЕ')
         elif call.data == 'search_cliche':
@@ -48,7 +56,6 @@ def main_operator(call):
         elif call.data == 'hedgehog':
             bot.send_message (call.message.chat.id, 'Жил-был ёжик, научился он дышать попой. Сел на пенёк и задохнулся') #Это единственная шутка которую я могу сходу рассказать.
 #Дальше блок чёрного списка, раньше был отдельной функцией. 
-
         elif call.data == 'show_blacklist':
             if len(blacklist) == 0:
                 bot.send_message (call.message.chat.id, 'Черный список пустой')
@@ -57,6 +64,8 @@ def main_operator(call):
         elif call.data == 'edit_blacklist':
                 keyboard_edit = types.InlineKeyboardMarkup()
                 key_add_words = types.InlineKeyboardButton(text = 'Добавить', callback_data = 'edit_add')
+                key_show_bl = types.InlineKeyboardButton(text='Посмотреть чёрный список', callback_data='show_blacklist' )
+                keyboard_edit.add (key_show_bl)
                 keyboard_edit.add(key_add_words)
                 key_delete_words = types.InlineKeyboardButton (text = 'Удалить', callback_data = 'delete_word')
                 keyboard_edit.add(key_delete_words)
@@ -64,7 +73,14 @@ def main_operator(call):
                 keyboard_edit.add(key_clear_words)
                 bot.send_message (call.message.chat.id, 'Редактирование чёрного списка', reply_markup = keyboard_edit)
         elif call.data == 'clear_blacklist':
-            clear_blacklist_function(blacklist)#не работает
-            bot.send_message (call.message.chat.id, 'Черный список очищен В РАЗРАБОТКЕ' + str(blacklist))
+            clear_blacklist_function(blacklist)
+            bot.send_message (call.message.chat.id, 'Черный список очищен')
+            clear_blacklist_function(blacklist)
+        elif call.data == 'edit_add':
+            bot.send_message (call.message.chat.id, 'Добавить слова в черный список В РАЗРАБОТКЕ')
+            add_words_bl_function(blacklist) 
+        elif call.data == 'delete_word':
+            bot.send_message (call.message.chat.id, 'Выборочно удалить слова из черного списка В РАЗРАБОТКЕ') 
+            
 
 bot.polling(non_stop = True)
